1c66e1ae6717cd9e32681ce98dc6a6fb75afd85b
Juan Simancas <dev.juansimancas@proton.me>
2025-06-06 12:12:07 -0500

    add swagger and open api specification for better auth

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,7 +1,8 @@
 import "dotenv/config";
 import { cors } from "@elysiajs/cors";
+import { swagger } from "@elysiajs/swagger";
 import { Elysia } from "elysia";
-import { auth } from "./lib/auth";
+import { auth, OpenAPI } from "./lib/auth";
 
 const app = new Elysia()
 	.use(
@@ -12,13 +13,15 @@ const app = new Elysia()
 			credentials: true,
 		}),
 	)
-	.all("/api/auth/*", async (context) => {
-		const { request } = context;
-		if (["POST", "GET"].includes(request.method)) {
-			return auth.handler(request);
-		}
-		context.error(405);
-	})
+	.mount("/auth", auth.handler)
+	.use(
+		swagger({
+			documentation: {
+				components: await OpenAPI.components,
+				paths: await OpenAPI.getPaths(),
+			},
+		}),
+	)
 	.get("/", () => "OK")
 	.listen(3000, () => {
 		console.log("Server is running on http://localhost:3000");
--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -1,9 +1,12 @@
 import { betterAuth } from "better-auth";
+import { openAPI } from "better-auth/plugins";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "../db";
 import * as schema from "../db/schema/auth";
 
 export const auth = betterAuth({
+	basePath: "/api",
+	plugins: [openAPI()],
 	database: drizzleAdapter(db, {
 		provider: "sqlite",
 
@@ -14,3 +17,33 @@ export const auth = betterAuth({
 		enabled: true,
 	},
 });
+
+let _schema: ReturnType<typeof auth.api.generateOpenAPISchema>;
+// biome-ignore lint/suspicious/noAssignInExpressions: <explanation>
+const getSchema = async () => (_schema ??= auth.api.generateOpenAPISchema());
+
+export const OpenAPI = {
+	getPaths: (prefix = "/auth/api") =>
+		getSchema().then(({ paths }) => {
+			const reference: typeof paths = Object.create(null);
+
+			for (const path of Object.keys(paths)) {
+				const key = prefix + path;
+				reference[key] = paths[path];
+
+				for (const method of Object.keys(paths[path])) {
+					// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+					const operation = (reference[key] as any)[method];
+
+					operation.tags = ["Better Auth"];
+				}
+			}
+
+			return reference;
+			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		}) as Promise<any>,
+	components: getSchema().then(
+		({ components }) => components,
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+	) as Promise<any>,
+} as const;
