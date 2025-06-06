9e0ac277b48b41e024d73101504e57580fd822ae
Juan Simancas <dev.juansimancas@proton.me>
2025-06-06 12:13:52 -0500

    update cors rules

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -8,7 +8,7 @@ const app = new Elysia()
 	.use(
 		cors({
 			origin: process.env.CORS_ORIGIN || "",
-			methods: ["GET", "POST", "OPTIONS"],
+			methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 			allowedHeaders: ["Content-Type", "Authorization"],
 			credentials: true,
 		}),
